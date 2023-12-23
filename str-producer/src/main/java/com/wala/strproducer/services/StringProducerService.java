25e1ff7a0f9678c736d9494e9249c66fed1a672d
walacealves <walace.alves10@gmail.com>
2023-12-23 18:14:27 -0300

    adiciona projeto para consumir as mensagens - configuração e listener

--- str-producer/src/main/java/com/wala/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/wala/strproducer/services/StringProducerService.java
@@ -12,17 +12,17 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
     public void sendMessage(String message) {
         log.info("Send mensage {}", message);
-        kafkaTemplate.send("str-topic", message)
-                .addCallback(
-                success -> {
-                    if (success != null) {
-                        log.info("Send message with sucess {}", message);
-                        log.info("Partition {}, Offset {}",
-                                success.getRecordMetadata().partition(),
-                                success.getRecordMetadata().offset());
-                    }
-                },
-                error -> log.error("Error send message")
-        );
+        kafkaTemplate.send("str-topic", message);
+//                .addCallback(
+//                success -> {
+//                    if (success != null) {
+//                        log.info("Send message with sucess {}", message);
+//                        log.info("Partition {}, Offset {}",
+//                                success.getRecordMetadata().partition(),
+//                                success.getRecordMetadata().offset());
+//                    }
+//                },
+//                error -> log.error("Error send message")
+//        );
     }
 }
